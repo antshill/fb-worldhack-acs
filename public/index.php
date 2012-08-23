06c8aa66f85d762002802d70c09e2cd7bd7f9285
BC <devworldhackaustin@wheresme.com>
2012-08-23 15:01:00 -0500

    Implemented home page and item page

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title>Austin Children's Shelter Wishes</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
@@ -15,19 +15,41 @@
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.6.1.min.js"></script>
+        <script src="http://code.jquery.com/jquery-1.8.0.js"></script>
+        <script>
+        	var appurl = "http://acs.fbworldhack.com";
+        	var apiurl = "http://acs-api.fbworldhack.com";
+        	function load(url) {
+        		$('#content').load(url);
+        	}
+        </script>
     </head>
     <body>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+        <script>
+          function money(v) {
+          	return '$' + Math.floor(v*100)/100.0;
+          }
+//        $.ready(function($) {
+        	$.ajax({
+        		url: apiurl + '/item',
+        		dataType: 'json',
+        		success: function(data, textStatus, jqXHR) {
+        			console.log(data, textStatus);
+        			$.each(data, function (i, item) {
+        			console.log(i, item);
+        				$('#needs').append('<tr/>');
+        				$('#needs tr:last').append('<td><a href="#' + item.id + '" onclick="load(appurl + \'/item.html\')">' + item.name + '</td>');
+        				$('#needs tr:last').append('<td style="text-align:right">' + money(item.cost) + '</td>');
+        				$('#needs tr:last').append('<td>' + item.status + '</td>');
+        			});
+        		}
+        	});
+//    	});
+        </script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
@@ -36,5 +58,14 @@
             g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
             s.parentNode.insertBefore(g,s)}(document,'script'));
         </script>
+        
+        <div id="content">
+			<h1>Wishes</h1>
+			<div>
+				<table id="needs">
+					<thead><th>Title</th><th>Cost</th><th>Status</th></thead>
+				</table>
+			</div>
+        </div>
     </body>
 </html>
